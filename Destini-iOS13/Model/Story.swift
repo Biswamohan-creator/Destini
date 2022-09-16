7e3e8dd78757c02ebd8d818ed84fd167a3d83b66
Biswamohan-creator <56292855+Biswamohan-creator@users.noreply.github.com>
2022-09-16 23:51:49 +0530

    1st commit

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Story.swift
+//  Destini-iOS13
+//
+//  Created by Biswamohan on 10/05/2022.
+//  Copyright © 2022 Mohan iStudios. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
